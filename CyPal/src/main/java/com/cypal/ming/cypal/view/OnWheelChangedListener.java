1f03f3dfa4d9241bb496917eaa86da4ef20c54a8
luoming <lan.sha@163.com>
2019-03-08 22:17:14 +0800

    修改包名

---
+++ CyPal/src/main/java/com/cypal/ming/cypal/view/OnWheelChangedListener.java
@@ -0,0 +1,5 @@
+package com.cypal.ming.cypal.view;
+
+public interface OnWheelChangedListener {
+	void onChanged(WheelView wheel, int oldValue, int newValue);
+}
---
+++ CyPal/src/main/java/com/cypal/ming/cypal/widgets/dialogs/DialogUtil.java
@@ -0,0 +1,10 @@
+package com.cypal.ming.cypal.widgets.dialogs;
+
+/**
+ * Created by wood121 on 2017/12/11.
+ */
+
+public class DialogUtil {
+
+
+}
